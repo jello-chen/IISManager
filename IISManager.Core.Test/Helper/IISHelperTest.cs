c2de8a869d17b6de53e9d830140ffa516cb35748
jello chen <jello_chen@live.com>
2018-05-16 07:58:20 +0800

    add unit test

--- IISManager.Core.Test/Helper/IISHelperTest.cs
+++ IISManager.Core.Test/Helper/IISHelperTest.cs
@@ -1,5 +1,8 @@
 ﻿using Xunit;
 using IISManager.Core.Helper;
+using System;
+using System.IO;
+using IISManager.Core.Utils;
 
 namespace IISManager.Core.Test.Helper
 {
@@ -15,6 +18,14 @@ namespace IISManager.Core.Test.Helper
         public void TestGetIISVersion()
         {
             Assert.True(IISHelper.GetIISVersion() == Domain.IISVersion.SevenOrUpper);
+        }
+
+        [Fact]
+        public void TestPublishXmlIsExisting()
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Publish");
+            ZipUtil.UnZip(Path.Combine(path, "Publish.zip"), path);
+            Assert.True(File.Exists(Path.Combine(path, "Publish.xml")));
         }
     }
 }
